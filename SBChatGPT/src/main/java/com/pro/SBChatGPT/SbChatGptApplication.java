7822d9b72e193caa2261bc0db2877013a6723f4b
prosenjit005 <33807029+prosenjit05@users.noreply.github.com>
2023-12-03 16:12:11 +0530

    initial commit

---
+++ SBChatGPT/src/main/java/com/pro/SBChatGPT/SbChatGptApplication.java
@@ -0,0 +1,13 @@
+package com.pro.SBChatGPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbChatGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbChatGptApplication.class, args);
+	}
+
+}
---
+++ SBChatGPT/src/test/java/com/pro/SBChatGPT/SbChatGptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pro.SBChatGPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class SbChatGptApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
